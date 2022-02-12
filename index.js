0c2849b2e4d21c99e327b8466f3c96259e9b6267
Jarosla Marhivka <jmarhivka@yahoo.com>
2022-02-12 21:05:18 +0200

    initial commit

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ app.listen(PORT, () => console.log(`server running on port ${PORT}`))
 
 app.get("/", (req, res) => res.json("Gonna be a LOL Esports API soon :)"))
 
-app.get(`/kek`, (req, res) => esportsService.hujeta(req, res))
+app.get("/get_region_standings_reg_season", (req, res) => esportsService.hujeta(req, res))
+
+app.get("/get_region_standings_playoffs", (req, res) => esportsService.hujeta(req, res))
